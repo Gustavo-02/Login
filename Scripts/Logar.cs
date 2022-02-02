c1fd93ab69d40cdfc633f505489d15c114a64902
Gustavo-02 <73145439+Gustavo-02@users.noreply.github.com>
2022-02-01 22:03:22 -0300

    Add files via upload

---
+++ Scripts/Logar.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Logar : MonoBehaviour {
+
+    private const string Login = "gustavo";
+    private const string Password = "guga2002";
+
+    [SerializeField]
+    private InputField usuarioField = null;
+    [SerializeField]
+    private InputField senhaField = null;
+    [SerializeField]
+    private Text feed = null;
+    [SerializeField]
+    private Toggle rememberData = null;
+    [SerializeField]
+    private GameObject load = null;
+    
+    public string ProximaCena;
+    void Start() 
+    {
+        load.SetActive(false);
+        if(PlayerPrefs.HasKey("lembra") && PlayerPrefs.GetInt("lembra") == 1){
+            usuarioField.text = PlayerPrefs.GetString("remeberUser");
+            senhaField.text = PlayerPrefs.GetString("remeberSenha");
+        }
+    }
+    
+    public void FazerOLogin() 
+    {
+       string usuario = usuarioField.text;
+       string senha = senhaField.text;
+
+       if(rememberData.isOn){
+           PlayerPrefs.SetInt("lembra", 1);
+           PlayerPrefs.SetString("remeberUser", usuario);
+           PlayerPrefs.SetString("remeberSenha", senha);
+       }
+
+       if(usuario == Login && senha == Password){
+           load.SetActive(true);
+           feed.CrossFadeAlpha(100f, 0f, false);
+           feed.color = Color.green;
+           feed.text = "Conectando...";
+           StartCoroutine(Carregando());
+       }
+       else{
+           load.SetActive(false);
+           feed.CrossFadeAlpha(100f, 0f, false);
+           feed.color = Color.red;
+           feed.text = "Usuario ou senha incorreto";
+           feed.CrossFadeAlpha(0f, 2f, false);
+           usuarioField.text = "";
+           senhaField.text = "";
+           usuarioField.Select();
+       }
+    }
+
+    public void Sair()
+    {
+        Application.Quit();
+    }
+   
+
+   IEnumerator Carregando()
+   {
+       yield return new WaitForSeconds(5);
+       SceneManager.LoadScene(ProximaCena);
+   }
+}
---
+++ Scripts/sair.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class sair : MonoBehaviour
+{
+    
+    public Button BotaoSair;
+    public string VoltarCena;
+
+    void Start()
+    {
+        BotaoSair.onClick = new Button.ButtonClickedEvent();
+      
+        BotaoSair.onClick.AddListener(() => Sair());
+    }
+    
+    void Update() 
+    {
+
+    }
+    
+    private void Sair() 
+    {
+        SceneManager.LoadScene(VoltarCena);
+    }
+}
